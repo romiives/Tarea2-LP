10f73233f3fc4df6952a8d27cbabf05d141b949e
Romina Valdés <valdeseromina@gmail.com>
2026-04-11 20:35:12 +0000

    arreglos errores

--- tablero.c
+++ tablero.c
@@ -1,4 +1,6 @@
 #include "tablero.h"
+#include <stdlib.h>
+#include <stddef.h>
 
 struct Tablero* tablero_crear(int ancho, int alto) {
     struct Tablero *t = malloc(sizeof(struct Tablero));
@@ -7,17 +9,16 @@ struct Tablero* tablero_crear(int ancho, int alto) {
     t->celdas = malloc(alto * sizeof (void**));
     for (int i=0; i<alto; i++) {
         t->celdas[i] = malloc(ancho * sizeof(void*));
-        for (int j=0, j<ancho; j++) {
+        for (int j=0; j<ancho; j++) {
             t->celdas[i][j] = NULL;
         }        
     }
     return t;
 }
 
-void tablero_imprimir(struct Juego *juego) {
+//void tablero_imprimir(struct Juego *juego) {
     Tablero *t = juego->t;
-
-}
+//}
 
 
 void tablero_liberar(struct Tablero *tablero) {
--- tablero.h
+++ tablero.h
@@ -1,3 +1,6 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
 typedef struct {
     int W, H;
     void ***celdas; /* celdas[y][x] apunta a Celda* */
@@ -5,3 +8,5 @@ typedef struct {
 struct Tablero* tablero_crear(int ancho, int alto);
 void tablero_imprimir(struct Juego *juego);
 void tablero_liberar(struct Tablero *tablero); /* debe limpiar la memoria heap */
+struct Juego;
+#endif 
