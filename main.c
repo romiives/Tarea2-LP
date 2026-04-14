b8f9be0d04ce6423b472e630b3b30c30906af76e
Romina Valdés <valdeseromina@gmail.com>
2026-04-14 22:45:21 +0000

    cambios

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main() {
     Tablero *t = tablero_crear(5, 5);
 
     printf("Tablero creado\n");
-
-    tablero_liberar(t);
     tablero_imprimir(t);
+    tablero_liberar(t);
+    
     return 0;
 }
