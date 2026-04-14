0a456a5d493657f4d28511698c7f2d1d62a6149e
Romina Valdés <valdeseromina@gmail.com>
2026-04-14 22:56:36 +0000

    cambios

--- main.c
+++ main.c
@@ -8,12 +8,12 @@ int main() {
     Tablero *t = tablero_crear(5, 5);
 
     Pieza *rey = malloc(sizeof(Pieza));
-    rey->tipo = 'R'
+    rey->tipo = 'R';
     rey->hp = 10;
     rey->x = 2;
     rey->y = 4;
     t->celdas[rey->y][rey->x] = rey;
-    
+
     printf("Tablero creado\n");
     tablero_imprimir(t);
     tablero_liberar(t);
