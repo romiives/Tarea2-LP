88a833c41f231ec1b8865ee0c4d21cef476f58c5
Romina Valdés <valdeseromina@gmail.com>
2026-04-14 22:55:26 +0000

    rey en tablero

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "tablero.h"
+#include <stdlib.h>
+#include "piezas.h"
 
 int main() {
 
     Tablero *t = tablero_crear(5, 5);
 
+    Pieza *rey = malloc(sizeof(Pieza));
+    rey->tipo = 'R'
+    rey->hp = 10;
+    rey->x = 2;
+    rey->y = 4;
+    t->celdas[rey->y][rey->x] = rey;
+    
     printf("Tablero creado\n");
     tablero_imprimir(t);
     tablero_liberar(t);
