8cbd21e4129c0210d82f8837970f24e01568f644
Romina Valdés <valdeseromina@gmail.com>
2026-04-18 14:36:57 +0000

    aparaciciòn del rey aleatorio

--- main.c
+++ main.c
@@ -2,17 +2,22 @@
 #include "tablero.h"
 #include <stdlib.h>
 #include "piezas.h"
+#include <time.h>
 
 int main() {
 
     Tablero *t = tablero_crear(12, 12);
 
+    srand(time(NULL));
     Pieza *rey = malloc(sizeof(Pieza));
     rey->tipo = 'R';
     rey->hp = 10;
-    rey->x = 2;
-    rey->y = 4;
-    t->celdas[rey->y][rey->x] = rey;
+    int x = rand() % (t->W-2)+1;
+    int y = t->H-1;
+    rey->x = x;
+    rey->y = y;
+    
+    t->celdas[y][x] = (void*) rey;
 
     printf("Tablero creado\n");
     tablero_imprimir(t);
