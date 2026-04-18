c24afc635be3157ecb5fc72c26e72a27cb0a13c3
Romina Valdés <valdeseromina@gmail.com>
2026-04-18 19:52:12 +0000

    arreglo de logica de tunos y peon

--- main.c
+++ main.c
@@ -46,6 +46,7 @@ int main() {
         //salida ddel juego
         if (letra == 'x'){ break; }
 
+        int accion_valida =0;
         //armas y su direccion
         if (letra >= '1' && letra <= '4'){
             int dx = 0, dy = 0;
@@ -61,34 +62,40 @@ int main() {
             if (direccion == 'z'){dx = -1; dy = 1;}
             if (direccion == 'c'){dx = 1; dy = 1;}
             j->arsenal.disparar[letra - '1'](j, dx, dy);
-            continue;
+            accion_valida = 1;
         }
 
         //mov del rey
-        int x_nueva = j->jugador->x;
-        int y_nueva = j->jugador->y;
-        if (letra == 'w') y_nueva--;
-        if (letra == 's') y_nueva++;
-        if (letra == 'a') x_nueva--;
-        if (letra == 'd') x_nueva++;
-        if (letra == 'q'){x_nueva--; y_nueva--;}
-        if (letra == 'e'){x_nueva++; y_nueva--;}
-        if (letra == 'z'){x_nueva--; y_nueva++;}
-        if (letra == 'c'){x_nueva++; y_nueva++;}
-        
-        if(x_nueva >=0 && x_nueva < j->t->W && y_nueva >= 0 && y_nueva < j->t->H){
-            j->t->celdas[j->jugador->y][j->jugador->x] = NULL;
-            j->jugador->x = x_nueva;
-            j->jugador->y = y_nueva;
-            j->t->celdas[j->jugador->y][j->jugador->x] = (void*) j->jugador;
+        if (letra == 'w' || letra == 'a' || letra == 's' || letra == 'd' || letra == 'q' || letra == 'e' || letra == 'z' || letra == 'c'){
+            int x_nueva = j->jugador->x;
+            int y_nueva = j->jugador->y;
+            if (letra == 'w') y_nueva--;
+            if (letra == 's') y_nueva++;
+            if (letra == 'a') x_nueva--;
+            if (letra == 'd') x_nueva++;
+            if (letra == 'q'){x_nueva--; y_nueva--;}
+            if (letra == 'e'){x_nueva++; y_nueva--;}
+            if (letra == 'z'){x_nueva--; y_nueva++;}
+            if (letra == 'c'){x_nueva++; y_nueva++;}
+            if(x_nueva >=0 && x_nueva < j->t->W && y_nueva >= 0 && y_nueva < j->t->H){
+                j->t->celdas[j->jugador->y][j->jugador->x] = NULL;
+                j->jugador->x = x_nueva;
+                j->jugador->y = y_nueva;
+                j->t->celdas[j->jugador->y][j->jugador->x] = (void*) j->jugador;
+            }
+            accion_valida =1;
         }
-
+        if (!accion_valida){
+            printf("Accion Invalida\n");
+            continue;
+        }
         //mov del pein
         mover_peon(j->t, peon, j->jugador);
 
         //peon choca con rey
         if (peon->x == j->jugador->x && peon->y == j->jugador->y){
             printf("El rey ha sido alcanzado\n");
+            j->t->celdas[j->jugador->y][j->jugador->x] = NULL;
             break;
         }
     }
