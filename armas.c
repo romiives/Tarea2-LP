c1111c99e02ab4f9c96c8b3d4e1eac63ad292aa7
Romina Valdés <valdeseromina@gmail.com>
2026-04-18 20:02:23 +0000

    arreglos de warnings temporal

--- armas.c
+++ armas.c
@@ -2,18 +2,30 @@
 #include "armas.h"
 
 bool escopeta(struct Juego *j, int dx, int dy){
+    (void)j;
+    (void)dx;
+    (void)dy;
     printf("Disparo escopeta(%d,%d)\n", dx, dy);
     return true;
 }
 bool francotirador(struct Juego *j, int dx, int dy){
+    (void)j;
+    (void)dx;
+    (void)dy;
     printf("Disparo sniper\n");
     return true;
 }
 bool granada(struct Juego *j, int x, int y){
+    (void)j;
+    (void)x;
+    (void)y;
     printf("Granada lanzada\n");
     return true;
 }
 bool especial(struct Juego *j, int dx, int dy){
     printf("Arma Especial\n");
+    (void)j;
+    (void)dx;
+    (void)dy;
     return true;
 }
